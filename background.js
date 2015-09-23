0baf8b49020dd345ebeb4b28ec63b7a67cf62504
tonyk <jinniw43805@gmail.com>
2015-09-23 10:46:25 +0800

    version1.7.2 add copy right

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'outerBounds': {
-      'width': 500,
+      'width':  700,
       'height': 500
     }
   });
